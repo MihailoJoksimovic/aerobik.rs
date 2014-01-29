5f4531e5f5c97e0aa0f752bdef89cffafeb1295c
Ikonic <ikonic.aleksandar@gmail.com>
2014-01-29 19:56:44 +0100

    HTML template added.

--- src/App/AerobicBundle/Controller/DefaultController.php
+++ src/App/AerobicBundle/Controller/DefaultController.php
@@ -6,8 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('AerobicBundle:Default:index.html.twig', array('name' => $name));
+        $userService = $this->get('user_service');
+        $user = $userService->getLoggedInUser();
+
+        $message = 'Hello bitch (you are not logged in, so you are a bitch).';
+        if (isset($user)) {
+            $message = 'Hello ' . $user->getUsername();
+        }
+        return $this->render('AerobicBundle:Default:index.html.twig', array('message' => $message));
     }
 }
